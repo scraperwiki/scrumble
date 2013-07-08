b25be1ceaf279a69361235454ac2ea356074c371
Dragon Dave <dragon@scraperwiki.com>
2013-07-08 11:00:33 +0100

    19

--- cleaner.py
+++ cleaner.py
@@ -1,3 +1,14 @@
+import re
+
+
+class NaN(unicode):
+    pass
+
+
+class NaNError(Exception):
+    pass
+
+
 def keeper(s):
     if not(isinstance(s, basestring)):
         return s
@@ -10,12 +21,33 @@ def keeper(s):
     return ''.join(new).strip()
 
 
+def brackets(s):
+    """
+    if there are brackets in a sensible place,
+    remove them and slap a - before it all.
+    """
+    bracketted = re.findall('(.*)\((.*)\)(.*)', s)
+    if len(bracketted) != 1:
+        if "(" in s or ")" in s:
+            raise NaNError(NaN(s))  # brackets, but not matching ones!
+        else:
+            return s  # do nothing
+    before, middle, after = bracketted[0]
+    if has_number(before) or has_number(after):
+        raise NaNError(NaN(s))  # numbers outside of brackets!
+    else:
+        return "-" + middle
+
+
 def has_number(s):
     return any(i in s for i in "0123456789")
-    
+
 
 def nocommas(s):
     return s.replace(',', '')
+
+def nospaceafterhyphen(s):
+    return re.sub('-\s*', '-', s)
 
 
 def is_int(s, strict=False):
@@ -40,6 +72,8 @@ def as_float(s, strict=False):
     if not has_number(s):
         return None
     print s
+    s = brackets(s)
     s = keeper(s)
     s = nocommas(s)
+    s = nospaceafterhyphen(s)
     return float(s)
