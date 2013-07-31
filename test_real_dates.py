d7b9583a30a6fba51c364acfb0a48ad09f08b83e
Dragon Dave <dragon@scraperwiki.com>
2013-07-31 12:12:09 +0100

    more real dates

--- test_real_dates.py
+++ test_real_dates.py
@@ -1,11 +1,12 @@
 from nose.tools import assert_equal
 import dates
-from dateutil.tz import tzoffset
 
 cases = [
-         ["1/1990", {'year':1990, 'month':1}],
-         ["31 January 2013", {'year': 2013, 'month':1, 'day':31}],
-        ]
+        ["1/1990", {'year': 1990, 'month': 1}],
+        ["31 January 2013", {'year': 2013, 'month': 1, 'day': 31}],
+        ["2012 10", {'year': 2012, 'month': 10}],
+        ["Mar 2012", {'year': 2012, 'month': 3}],
+]
 
 
 def test_basic():
@@ -15,4 +16,3 @@ def test_basic():
 
 def do_basic(p):
     return assert_equal(p[1], dict(dates.as_date(p[0])))
- 
