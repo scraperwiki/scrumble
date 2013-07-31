7499c19e74ec6c18cfcccde37259866cec906d39
Dragon Dave <dragon@scraperwiki.com>
2013-07-31 15:04:34 +0100

    setup

--- setup.py
+++ setup.py
@@ -1,5 +1,33 @@
-from distutils.core import setup
-setup(name='scrumble',
-              version='0.0.1',
-                    py_modules=[],
-                          )
+from setuptools import setup, find_packages
+
+long_desc = """TODO"""
+
+
+setup(
+    name='scrumble',
+    version='0.0.1',
+    description="Get clean numbers, dates etc. from text",
+    long_description=long_desc,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+    ],
+    keywords='',
+    author='ScraperWiki',
+    author_email='feedback@scraperwiki.com',
+    url='http://scraperwiki.com',
+    license='BSD',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    namespace_packages=[],
+    include_package_data=False,
+    zip_safe=False,
+    install_requires=[
+        'python-dateutil>=1.5.0,<2.0.0',
+    ],
+    extras_require={},
+    tests_require=[],
+    entry_points="",
+)
